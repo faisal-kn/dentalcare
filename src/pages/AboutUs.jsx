7dee70352467fef46e5c910c953d9fd273873929
faisal-kn <khanmohdfaisal75@gmail.com>
2022-03-20 19:30:30 +0530

    added visit us component

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,6 +6,7 @@ import { BiTimeFive } from "react-icons/bi";
 import doctor from "../assets/doctor.jpg";
 import about from "../assets/about.jpg";
 import VisitUs from "../components/VisitUs";
+import doctor1 from "../assets/doctor1.jpg";
 
 const dummyData = [" empathy", " integrity", " gratitude"];
 const AboutUs = () => {
@@ -255,11 +256,15 @@ const AboutUs = () => {
             </Row>
           </Col>
         </Row>
-        <VisitUs/>
-        {/* <Row>
-          <Col></Col>
-          <Col>
-            <h2>Visit Us</h2>
+        {/* <VisitUs/> */}
+        <Row style={{ margin: "20px" }}>
+          <Col md={12} lg={5}>
+            <div class={styles.visitcontainer}>
+              <img src={doctor1} alt="a doctor" className={styles.imageUs} />
+              <div class={styles["top-right"]}>VISIT US</div>
+            </div>
+          </Col>
+          <Col md={12} lg={7}>
             <p>
               We’re conveniently located in the Denver Tech Center area. Find us
               on the 4th floor of the Triad Office Park building. Plenty of free
@@ -278,7 +283,7 @@ const AboutUs = () => {
         </Row>
         <Row className={styles.mapContainer}>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7756.576108193791!2d80.04318612605061!3d13.579204961837346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4d76fcef996f57%3A0x645caee60af97355!2sPudi%2C%20Andhra%20Pradesh%20524401!5e0!3m2!1sen!2sin!4v1646929611895!5m2!1sen!2sin"></iframe>
-        </Row> */}
+        </Row>
       </Container>
     </>
   );
